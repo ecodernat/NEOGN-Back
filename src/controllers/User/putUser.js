391f480db97fcacc8562b6c2e5d089e5c6243a3f
Alan <alanjesuseliasruiz@gmail.com>
2023-10-16 17:20:19 -0300

    get user

--- src/controllers/User/putUser.js
+++ src/controllers/User/putUser.js
@@ -1,13 +1,9 @@
-const bcryptjs = require("bcryptjs");
 const db = require("../../db");
 
-const updateUser = async (id, data) => {
-  const user = await db.User.findByPk(id);
+const updateUser = async (clientId, data) => {
+  const user = await db.User.findOne({ where: { clientId } });
 
   if (!user) throw new Error("User not found");
-
-  if (data.password && data.password !== user.password)
-    data.password = await bcryptjs.hash(password, 10);
 
   await user.update({ ...user, ...data });
 
