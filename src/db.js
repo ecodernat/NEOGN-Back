ac5d54799dca45e87a3e8b9856d63fc3898e95a5
Alan <alanjesuseliasruiz@gmail.com>
2023-10-31 18:35:09 -0300

    sin deploy

--- src/db.js
+++ src/db.js
@@ -6,12 +6,15 @@ const paymentModel = require("./models/Payment");
 const productModel = require("./models/Product");
 const userModel = require("./models/User");
 
-const { DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false,
-  native: false,
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/neogn`,
+  {
+    logging: false,
+    native: false,
+  }
+);
 
 orderModel(sequelize);
 paymentModel(sequelize);
