23732e4520bb70c7813122745475bd02f086999b
Alan Ruiz <alanjesuseliasruiz@gmail.com>
2023-11-22 17:43:26 -0300

    testing deploy

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const mercadopago = require("mercadopago");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const compression = require("compression");
 
 const router = require("./routes");
@@ -13,12 +13,12 @@ mercadopago.configure({
   access_token: process.env.MP_TOKEN,
 });
 
-server.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "https://http2.mlstatic.com"],
-  },
-}));
+// server.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", "https://http2.mlstatic.com"],
+//   },
+// }));
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(morgan("dev"));
